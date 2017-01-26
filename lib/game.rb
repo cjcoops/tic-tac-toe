1a7c1b5bba222c2cca83d9e56eecb5fc7fb89ece
Chris Cooper <cjcooper252@gmail.com>
2017-01-26 11:21:42 +0000

    a player mark switches the turn

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,23 @@ require_relative "player"
 
 class Game
 
-  attr_reader :playerX, :playerO, :turn
+  attr_reader :playerX, :playerO
+  attr_accessor :turn
 
   def initialize(player: Player)
     @playerX = Player.new(mark: :X)
     @playerO = Player.new(mark: :O)
-    @turn = playerX
+    @turn ||= playerX
+  end
+
+  def mark
+    switch_turns
+  end
+
+  private
+
+  def switch_turns
+    turn == playerX ? self.turn = playerO : self.turn = player1
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,11 +9,23 @@ describe Game do
     expect(game).to respond_to(:playerO)
   end
 
-  it "playerO goes first" do
+  it "playerX goes first" do
     expect(game.turn).to equal(game.playerX)
   end
 
+  it "playerO goes next" do
+    game.mark
+    expect(game.turn).to equal(game.playerO)
+  end
 
+  describe "#mark" do
+
+    it "switches the turn" do
+      game.mark
+      expect(game.turn).to equal(game.playerO)
+    end
+
+  end
 
 
 end
